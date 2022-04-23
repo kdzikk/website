433959b35bb166ad373f73c900107efe7b88199b
kdzikk <103208283+kdzikk@users.noreply.github.com>
2022-04-23 17:46:58 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,21 @@
-const nav = document.querySelector('.navbar')
-const navList = document.querySelector('.navbar-collapse')
+document.addEventListener('DOMContentLoaded', function () {
+	const nav = document.querySelector('.navbar')
+	const navList = document.querySelector('.navbar-collapse')
+	const toTheTopBtn = document.querySelector('.to-the-top')
 
-document.onclick = function (e) {
-	if (e.target.id !== 'navbar') {
-		navList.classList.remove('show')
+	document.onclick = function (e) {
+		if (e.target.id !== 'navbar') {
+			navList.classList.remove('show')
+		}
 	}
-}
+
+	const hideBtn = () => {
+		if (window.scrollY > 300) {
+			toTheTopBtn.style.display = 'flex'
+			toTheTopBtn.classList.add('btnEntrance')
+		} else {
+			toTheTopBtn.style.display = 'none'
+		}
+	}
+	window.addEventListener('scroll', hideBtn)
+})
